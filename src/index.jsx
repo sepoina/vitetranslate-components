29777541707ec2d64107f4698c8145cbf3fc1fcc
sepoina <sepoina@gmail.com>
2024-01-01 14:18:00 +0100

    remove rollup

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,8 @@
-// export * from './components/Rating'
-import React from "react";
 import TranslateContainer from "./components/TranslateContainer.jsx";
 import Translate from "./components/Translate.jsx";
 import { TranslateContext } from "./components/TranslateContext";
-import babelTranslate from "./dev/babel/babelTranslate";
-import rollupTranslate from "./dev/rollup/rollupTranslate";
 
 export {
-  babelTranslate, rollupTranslate,
   TranslateContainer, TranslateContext, Translate
 };
 
